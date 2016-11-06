94cd2200bc81b4d5255698f93cece571077db8ec
Holger <hdecarne@gmail.com>
2016-11-06 13:40:12 +0100

    AboutInfo Dialog added.

---
+++ src/main/java/de/carne/jfx/scene/control/aboutinfo/AboutInfoController.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014-2016 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.jfx.scene.control.aboutinfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import de.carne.jfx.scene.control.DialogController;
+import de.carne.util.AboutInfo;
+import de.carne.util.Exceptions;
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * About info window.
+ */
+public class AboutInfoController extends DialogController<ButtonType> {
+
+	@FXML
+	TabPane ctlInfoTabs;
+
+	@Override
+	protected void setupDialog(Dialog<ButtonType> dialog) {
+		dialog.setTitle(AboutInfoI18N.formatSTR_STAGE_TITLE(AboutInfo.PROJECT_ID));
+		dialog.setHeaderText(AboutInfoI18N.formatSTR_TEXT_HEADER(AboutInfo.PROJECT_NAME, AboutInfo.PROJECT_VERSION,
+				AboutInfo.PROJECT_BUILD));
+	}
+
+	/**
+	 * Set the application logo.
+	 *
+	 * @param image The logog image to display.
+	 * @return This controller.
+	 */
+	public AboutInfoController setLogo(Image image) {
+		assert image != null;
+
+		getUI().setGraphic(new ImageView(image));
+		return this;
+	}
+
+	/**
+	 * Add an info message to the dialog.
+	 * <p>
+	 * The first line of the submitted text resource is used as a title. The
+	 * remaining lines are displayed as a single text.
+	 * <p>
+	 * If the submitted URL is {@code null} or cannot be read, no info message
+	 * is added to the dialog.
+	 *
+	 * @param url The URL of the text resource to add.
+	 * @return This controller.
+	 */
+	public AboutInfoController addInfo(URL url) {
+		if (url != null) {
+			try (BufferedReader infoReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+				String infoTitle = infoReader.readLine();
+				StringBuilder infoTextBuffer = new StringBuilder();
+				String infoTextLine;
+
+				while ((infoTextLine = infoReader.readLine()) != null) {
+					infoTextBuffer.append(infoTextLine).append('\n');
+				}
+
+				TextArea infoTextArea = new TextArea(infoTextBuffer.toString());
+
+				infoTextArea.setEditable(false);
+
+				AnchorPane infoTextPane = new AnchorPane(infoTextArea);
+
+				AnchorPane.setLeftAnchor(infoTextArea, 0.0);
+				AnchorPane.setTopAnchor(infoTextArea, 0.0);
+				AnchorPane.setRightAnchor(infoTextArea, 0.0);
+				AnchorPane.setBottomAnchor(infoTextArea, 0.0);
+				this.ctlInfoTabs.getTabs().add(new Tab(infoTitle, infoTextPane));
+			} catch (IOException e) {
+				Exceptions.ignore(e);
+			}
+		}
+		return this;
+	}
+
+}
---
+++ src/main/java/de/carne/jfx/scene/control/aboutinfo/AboutInfoDialog.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014-2016 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.jfx.scene.control.aboutinfo;
+
+import java.io.IOException;
+
+import de.carne.jfx.stage.StageController;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+
+/**
+ * About info dialog.
+ */
+public class AboutInfoDialog extends Dialog<ButtonType> {
+
+	private AboutInfoDialog(AboutInfoController controller) {
+		// Make sure this class is not instantiated from outside
+	}
+
+	/**
+	 * Load the about info dialog.
+	 *
+	 * @param owner The stage controller owning this dialog.
+	 * @return The constructed controller which is bound to the newly created
+	 *         dialog.
+	 * @throws IOException if an I/O error occurs during dialog loading.
+	 */
+	public static AboutInfoController load(StageController owner) throws IOException {
+		assert owner != null;
+
+		return owner.loadDialog((c) -> new AboutInfoDialog(c), AboutInfoController.class);
+	}
+
+}
