13f27990ccd4c06c41ed3dcc153c3dd7b8aa856f
Holger <hdecarne@gmail.com>
2016-11-26 18:33:35 +0100

    Basic tooltip support added.

---
+++ src/main/java/de/carne/jfx/scene/control/Tooltips.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2014-2016 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.jfx.scene.control;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import javafx.geometry.Point2D;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+import javafx.stage.PopupWindow.AnchorLocation;
+
+/**
+ * Utility class providing {@link Tooltip} related functions.
+ */
+public final class Tooltips {
+
+	private Tooltips() {
+		// Make sure this class is not instantiated from outside
+	}
+
+	/**
+	 * Show a tool tip for a specific region.
+	 *
+	 * @param region The region to display the tool tip for.
+	 * @param message The message to display:
+	 * @return The created tool tip.
+	 */
+	public static Tooltip show(Region region, String message) {
+		return show(region, message, null);
+	}
+
+	/**
+	 * Show a tool tip for a specific region.
+	 *
+	 * @param region The region to display the tool tip for.
+	 * @param message The message to display:
+	 * @param image The image to display. May be {@code null}.
+	 * @return The created tool tip.
+	 */
+	public static Tooltip show(Region region, String message, @Nullable Image image) {
+		assert region != null;
+		assert message != null;
+
+		Tooltip tooltip = new Tooltip(message);
+
+		if (image != null) {
+			tooltip.setGraphic(new ImageView(image));
+		}
+		tooltip.setAnchorLocation(AnchorLocation.WINDOW_TOP_LEFT);
+		tooltip.setAutoHide(true);
+
+		Point2D position = region.localToScreen(0.0, region.getHeight());
+
+		tooltip.show(region.getScene().getWindow(), position.getX(), position.getY());
+		return tooltip;
+	}
+
+}
